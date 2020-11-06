2aca8af72c3474a432ee60e45b32fe4b7c6e8a59
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-11-06 15:24:07 -0700

    new paths and views for about
    added the paths to get to author and site about pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/site/', views.about_site, name='about_site'),
+    path('about/author/', views.about_author, name='about_author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def about_site(request):
+    return render(request, 'about_site.html')
+
+
+def about_author(request):
+    return render(request, 'about_author.html')
