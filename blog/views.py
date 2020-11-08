3a5a1f989904ddc0e75672aba1f8fee235a41c3f
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-11-07 20:51:58 -0700

    pulled header and footer from base . html
    cleaning effort to simplify the inheritance of base.html and allow rapid iteration of additional content to header and footer.

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    page_header = {'page_header': "Home Page"}
+    return render(request, 'index.html', page_header)
 
 
 def about_site(request):
-    return render(request, 'about_site.html')
+    page_header = {'page_header': "About orbitaladd.com"}
+    return render(request, 'about_site.html', page_header)
 
 
 def about_author(request):
-    return render(request, 'about_author.html')
+    page_header = {'page_header': "About the Developer"}
+    return render(request, 'about_author.html', page_header)
