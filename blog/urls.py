81ace31e0b4561a850e7b74c39c1f6d76b07c620
KevinBRitter <ritter_kevin_b@hotmail.com>
2020-11-09 00:31:45 -0700

    refactoring the home page
    index changed to dashboard

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.dashboard, name='dashboard'),
     path('about/site/', views.about_site, name='about_site'),
     path('about/author/', views.about_author, name='about_author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
+def dashboard(request):
     page_header = {'page_header': "Home Page"}
-    return render(request, 'index.html', page_header)
+    return render(request, 'dashboard.html', page_header)
 
 
 def about_site(request):
