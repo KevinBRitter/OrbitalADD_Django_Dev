5e2006ab24244bd73147b2af5134bbfc327ad773
KevinBRitter <ritter_kevin_b@hotmail.com>
2021-01-04 14:04:01 -0700

    adding edits to blog posts
    new views, urls, and pages dedicated to adding new blog posts and editing old ones.

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
 
     path('blog/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
